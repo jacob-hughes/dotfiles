82b43315eeef2ebd05522842dc8ac42b3ed22934
Jake Hughes <jh@jakehughes.uk>
2024-03-15 17:08:37 +0000

    Fix right prompt to show hostname

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,16 +14,9 @@ alias vim='nvim'
 function fish_right_prompt -d "Write out the right prompt"
   set -l exit_code $status
 
-  if set -q $SSH_CLIENT || set -q $SSH_TTY
+  if set -q SSH_CLIENT || set -q SSH_TTY
     set_color brblack
-    echo (whoami)@$hostname
-    set_color normal
-  end
-
-  # Print a red dot for failed commands.
-  if test $exit_code -ne 0
-    set_color red
-    echo -n "• "
+    echo (whoami)@$hostname" "
     set_color normal
   end
 end
