74742d6a02973fab094bacd87e9da19e87b38b54
Jake Hughes <jh@jakehughes.uk>
2025-02-17 18:10:24 +0000

    Remote paste in vim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -27,4 +27,24 @@ require("lazy").setup({
 require("options")
 require("mappings")
 
+-- Define copy and paste functions for OSC52
+local function copy(lines, _)
+    local text = table.concat(lines, "\n")
+    local osc52 = "\x1b]52;c;" .. vim.fn.system("base64", text):gsub("\n", "") .. "\x07"
+    vim.fn.chansend(vim.v.stderr, osc52)
+end
+
+local function paste()
+    return {vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('')}
+end
+
+-- Set up the clipboard provider to use OSC52
+vim.g.clipboard = {
+    name = "osc52",
+    copy = {["+"] = copy, ["*"] = copy},
+    paste = {["+"] = paste, ["*"] = paste},
+}
+
+-- Optional: Use the '+' register by default for yanking and pasting
+vim.opt.clipboard = "unnamedplus"
 
